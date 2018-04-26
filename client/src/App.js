be93db70e6855f90e771d98f27bc7ad9242ac436
mariastenquist <stenquistm@gmail.com>
2018-04-25 19:16:07 -0600

    added plane model and resolved client build issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 // import React, { Component } from 'react';
 import React from 'react'
+import { Button, Icon, Modal } from 'react-materialize'
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 
 // import Login from "./pages/LandingPage";
-
 // class App extends Component {
 //   render() {
 //     return (
@@ -33,7 +33,13 @@ const App = () => (
         <Route exact path="/charts/:id" component={Specific-Flight-Detail} />
       <Route component={NoMatch} /> */}
       </Switch>
-      <h2>Welcome to FlightBuddy</h2>
+      <h4>Welcome to FlightBuddy</h4>
+          <Modal
+            header='Login:'
+            trigger={<Button waves='light'>Sign Up For Flight Buddy!<Icon right>insert_chart</Icon></Button>}>
+            <p> This is where we can put a sign-up for for the user to login to view Flight Buddy product. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.</p>
+          </Modal>
     </div>
   </Router>
   </div>
