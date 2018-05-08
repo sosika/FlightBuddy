ff4ba3e6c3891fef30695a29e50da756822ca4f0
mariastenquist <stenquistm@gmail.com>
2018-05-08 11:23:27 -0600

    gitignore update

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-module.exports = "mongodb://mariastenquist:Dinfart##@ds217310.mlab.com:17310/heroku_zwdsmx76"
+module.exports = "mongodb://flightBuddyDB:Dinfart##@ds217310.mlab.com:17310/heroku_zwdsmx76"
 
 // mongodb://<dbuser>:<dbpassword>@ds217310.mlab.com:17310/heroku_zwdsmx76
 //check dbuser name
