86897315ef78f9b81721efaef6b8359478c92acc
mariastenquist <stenquistm@gmail.com>
2018-05-08 11:26:11 -0600

    change config

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = "mongodb://flightBuddyDB:Dinfart##@ds217310.mlab.com:17310/heroku_zwdsmx76"
-
-// mongodb://<dbuser>:<dbpassword>@ds217310.mlab.com:17310/heroku_zwdsmx76
-//check dbuser name
